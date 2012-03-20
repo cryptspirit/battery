e232e0d8a8dcb2b58453ad43945d7fc2cc04c186
Maxim Podlesnyj <cryptspirit@gmail.com>
2012-03-20 11:38:57 +0200

    Использование информации из /sys. Первый прототип

--- battery_icon.py
+++ battery_icon.py
@@ -31,25 +31,45 @@ class ACPI_Parser():
     '''
     Класс чтения информации из acpi
     '''
-    def read(self):
-        return self.__pars_out__(subprocess.Popen(['acpi', '-b'], stdout=subprocess.PIPE).communicate()[0])
+    path_to_bat = '/sys/class/power_supply'
+    bat_name = 'BAT0'
     
-    def __pars_out__(self, stdout):
+    def read(self):
+        return self.__pars_out_new__()
+    
+    def __pars_out_new__(self):
         '''
-        Чтение строки получиной от утилиты acpi
+        Получения данных из /sys
         '''
+        status = {'energy_now': 0, 'energy_full': 0, 'power_now': 0, 'energy_full_design': 0, 'status': 0}
         return_battery_array = {}
-        battery_array = stdout.split('\n')
-        for i in battery_array[:-1]:
-            sep_index = i.find(':')
-            battery = i[:sep_index]
-            return_battery_array[battery] = {}
-            a = i[sep_index + 1:].split(',')
-            return_battery_array[battery]['Status'] = re.search(r'\S+', a[0]).group()
-            return_battery_array[battery]['Stat'] = int(re.search(r'\d+', a[1]).group())
-            return_battery_array[battery]['Time'] = re.search(r'\d+:\d+:\d+', a[2]).group()
-        return return_battery_array
-        
+        bat_dir = os.path.join(self.path_to_bat, self.bat_name)
+        if os.path.isdir(bat_dir):
+            for i in status.keys():
+                f = open(os.path.join(bat_dir, i))
+                status_value = f.read()
+                f.close()
+                if i == 'status':
+                    if status_value.find('Charging') == 0: # зарядка
+                        status[i] = 1
+                    elif status_value.find('Discharging') == 0: # работа от батареи
+                        status[i] = -1
+                    else:
+                        status[i] = 0
+                else:
+                    status[i] = int(status_value)
+                
+            return_battery_array['Stat'] = int(float(status['energy_now']) * 100 / status['energy_full'])
+            return_battery_array['Status'] = status['status']
+            # time
+            h = str(int(float(status['energy_now']) / status['power_now']))
+            m = str(int(((float(status['energy_now']) % status['power_now']) / status['power_now']) * 60))
+            return_battery_array['Time'] = '%s:%s' % (h.zfill(2), m.zfill(2))
+            
+            return_battery_array['Diff_design'] =  int(float(status['energy_full']) * 100 / status['energy_full_design'])
+            return return_battery_array
+        else:
+            return None
             
 class Battery_Icon(gtk.StatusIcon):
     '''
@@ -80,7 +100,7 @@ class Battery_Icon(gtk.StatusIcon):
         Формирование строки имени иконки
         '''
         plugged = ''
-        if battery['Status'] == 'Charging': plugged = '-plugged'
+        if battery['Status'] == 1: plugged = '-plugged'
         if battery['Stat'] <= 100 and battery['Stat'] >= 90:
             prc = '100'
         elif battery['Stat'] < 90 and battery['Stat'] >= 70:
@@ -101,28 +121,31 @@ class Battery_Icon(gtk.StatusIcon):
         '''
         now = []
         while self.exit_flag:
-            now_old = now    
+            now_old = now  
             now = self.ACPI.read()
-            now = now[now.keys()[0]] # Чтение только первого ключа
-            if now != now_old:
-                gtk.gdk.threads_enter()
-                thm = gtk.icon_theme_get_default()
-                pixb = thm.load_icon(self.make_icon_file_name(now),
-                                    22, gtk.ICON_LOOKUP_USE_BUILTIN)
+            if not now:
+                pixb = gtk.icon_theme_get_default().load_icon(\
+        'gpm-battery-missing', 22, gtk.ICON_LOOKUP_USE_BUILTIN)
                 self.set_from_pixbuf(pixb)
-                if now['Status'] == 'Charging':
-                    self.set_tooltip('Зарядка %s осталось %s' % (now['Stat'], now['Time']))
-                else:
-                    self.set_tooltip('Емкость %s осталось %s' % (now['Stat'], now['Time']))
-                #if now[3]:
-                #    subprocess.Popen(['notify-send', now[3]], stderr=None, shell=True)
-                gtk.gdk.threads_leave()
-            
-                for i in xrange(4):
-                    if self.exit_flag:
-                        time.sleep(1)
+                self.set_tooltip('Батарея отсутствует')
+            else:
+                if now != now_old:
+                    gtk.gdk.threads_enter()
+                    thm = gtk.icon_theme_get_default()
+                    pixb = thm.load_icon(self.make_icon_file_name(now),
+                                        22, gtk.ICON_LOOKUP_USE_BUILTIN)
+                    self.set_from_pixbuf(pixb)
+                    if now['Status'] == 1:
+                        self.set_tooltip('Зарядка %s осталось %s ресурс %s' % (now['Stat'], now['Time'], now['Diff_design']))
                     else:
-                        break
+                        self.set_tooltip('Емкость %s осталось %s ресурс %s' % (now['Stat'], now['Time'], now['Diff_design']))
+                    gtk.gdk.threads_leave()
+                
+            for i in xrange(4):
+                if self.exit_flag:
+                    time.sleep(1)
+                else:
+                    break
     
     def menu(self, icon, event_button, event_time):
         '''
